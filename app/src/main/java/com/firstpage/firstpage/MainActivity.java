9925edb833b52f947ee8d7dec18339be27989843
Sobol-R <rostislav.sobolev@gmail.com>
2018-10-01 18:37:49 +0300

    animation

--- app/src/main/java/com/firstpage/firstpage/MainActivity.java
+++ app/src/main/java/com/firstpage/firstpage/MainActivity.java
@@ -2,13 +2,41 @@ package com.firstpage.firstpage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+
+    Animation leftToRight;
+    Animation rightToLeft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = findViewById(R.id.main_text);
+        button = findViewById(R.id.nextButton);
+
+        leftToRight = AnimationUtils.loadAnimation(this, R.anim.left_to_right);
+        rightToLeft = AnimationUtils.loadAnimation(this, R.anim.right_to_left);
+        leftToRight.setDuration(1000);
+        rightToLeft.setDuration(1000);
+        textView.startAnimation(leftToRight);
+        button.startAnimation(rightToLeft);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.startAnimation(leftToRight);
+                button.startAnimation(rightToLeft);
+            }
+        });
     }
 }
